d802cfe1d7c025d528f8689d7f905234d64d008a
nilsonmolina <nilson13@me.com>
2019-01-30 20:00:05 -0800

    rough draft done... need to fix responsive styles

---
+++ chapp/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+cd react-app/
+sed -i -e 's/127.0.0.1/23.23.42.188/g' ./src/App.js
+npm run build
+rm -rf ../server/public/
+cp -r build/ ../server/public/
+cd ../server
+
+tar czf chapp.tar.gz server.js package.json package-lock.json ecosystem.config.js public
+scp -r -i ~/.ssh/aws-kp-2018-02-05.pem chapp.tar.gz ubuntu@23.23.42.188:~
+rm chapp.tar.gz
+
+ssh -i ~/.ssh/aws-kp-2018-02-05.pem ubuntu@23.23.42.188 << 'ENDSSH'
+pm2 delete chapp
+rm -rf chapp
+mkdir chapp
+tar xf chapp.tar.gz -C chapp
+rm chapp.tar.gz
+cd chapp
+npm install
+mkdir logs
+pm2 start
+ENDSSH
---
+++ chapp/server/public/precache-manifest.784cc40c3eb414492447f4ef7592ee2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5aa351b830f9eb20b0d",
+    "url": "/static/js/main.f5aa351b.chunk.js"
+  },
+  {
+    "revision": "cad093215162c471476c",
+    "url": "/static/js/1.cad09321.chunk.js"
+  },
+  {
+    "revision": "f5aa351b830f9eb20b0d",
+    "url": "/static/css/main.167171b7.chunk.css"
+  },
+  {
+    "revision": "116792995da065624a403bb6b233d29a",
+    "url": "/index.html"
+  }
+];
